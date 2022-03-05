ef6a46887e99c20c76901cd683718bc0997540ad
studentcarlo <ollerescarlo@gmail.com>
2022-03-05 21:52:53 +0800

    first test for CI

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React V2
+          Learn React V3
         </a>
       </header>
     </div>
