5c11cfcc16ed0dbee8696ed83a990043ce1b91c4
studentcarlo <ollerescarlo@gmail.com>
2022-03-05 22:10:07 +0800

    agar wood

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React V3
+          Learn React V4
         </a>
       </header>
     </div>
